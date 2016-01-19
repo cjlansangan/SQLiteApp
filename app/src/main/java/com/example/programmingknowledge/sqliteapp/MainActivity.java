fccd1780dc9e95e9526bde8f0485c4dbcf0fc153
Celdane James Lansangan <jameslansangan2@gmail.com>
2016-01-19 09:27:48 +0800

    adding comment

--- app/src/main/java/com/example/programmingknowledge/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/programmingknowledge/sqliteapp/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         viewAll();
         UpdateData();
         DeleteData();
-
+        // Quijoy
             }
 
     public void DeleteData(){
